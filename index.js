8582dee5c441969779a25f4bff3906dec97c2308
bradley <fazebrad13@gmail.com>
2025-09-25 22:22:03 +0300

    last commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer= "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer="maybe bob";
+}
+
+const leastFavoriteCustomer = "Ismail";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "not Ismail";
+}
